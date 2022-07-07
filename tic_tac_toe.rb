3247b2817c2fcebfb1d42fa8216a83e0977e9392
Jee El <98971420+Jee-El@users.noreply.github.com>
2022-07-07 16:38:04 +0100

    Feat: Add a multiplayer mode
    This also, hopefully, implements OOP in a better way than before.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,4 +1,5 @@
-module Game
+# User input interpreter & possible wins
+module Settings
   POSITIONS = {
     1 => 2,
     2 => 6,
@@ -18,86 +19,156 @@ module Game
                       [3, 6, 9],
                       [1, 5, 9],
                       [3, 5, 7]].freeze
-  @@board = "\n \s |\s \s|\s \n\n  \s|\s \s|\s \n\n \s |\s \s|\s \n\n"
+end
+
+# Game launch/start
+class Game
+  include Settings
+
+  def initialize
+    @first_player = Human.new
+
+    build_players
+
+    puts 'Please enter a number between 1 and 9'
+
+    @is_single_player ? single_player_game : multiplayer_game
+
+    report_results
+  end
+
+  def build_players
+    loop do
+      puts 'Choose by typing the corresponding number :'
+      puts "1 => Single Player,\n2 => Multiplayer"
+      @game_type = gets.chomp
+      case @game_type
+      when '1'
+        @is_single_player = true
+        @second_player = Computer.new
+        break
+      when '2'
+        @is_single_player = false
+        @second_player = Human.new
+        break
+      end
+    end
+  end
+
+  def single_player_game
+    until @first_player.wins? || @second_player.wins? || Player.ties?
+      @first_player_move = gets.chomp.to_i
+      if @first_player.make_a_move(@first_player_move, 'X') && !@first_player.wins? && !Player.ties?
+        @second_player.make_a_move
+      end
+      puts Player.board
+    end
+  end
+
+  def multiplayer_game
+    until @first_player.wins? || @second_player.wins? || Player.ties?
+      @first_player_move = gets.chomp.to_i
+      until @first_player.make_a_move(@first_player_move, 'X')
+        puts Player.board
+        @first_player_move = gets.chomp.to_i
+      end
+      puts Player.board
+      if !@first_player.wins? && !Player.ties?
+        @second_player_move = gets.chomp.to_i
+        until @second_player.make_a_move(@second_player_move, 'O')
+          puts Player.board
+          @second_player_move = gets.chomp.to_i
+        end
+      end
+      puts Player.board
+    end
+  end
+
+  def report_results
+    if @first_player.wins?
+      puts "Ez win for #{@first_player.player_name}"
+    elsif @second_player.wins?
+      puts "Ez win for #{@second_player.player_name}"
+    else
+      puts 'A Tie!'
+    end
+  end
+end
+
+# General settings for any player
+class Player < Game
+  attr_reader :player_name
+
+  @@board = "\n\s\s\s|\s\s\s|\s\s\n\n\s\s\s|\s\s\s|\s\s\n\n\s\s\s|\s\s\s|\s\s\n\n"
+  @@possible_moves = [*(1..9)]
+
+  def initialize
+    @past_moves = []
+  end
 
   def self.board
     @@board
   end
 
-  # General settings for any player
-  class Player
-    @@possible_moves = [*(1..9)]
-
-    def initialize
-      @past_moves = []
-    end
-
-    def make_a_move(move)
-      return unless legal_move?(move)
-
-      save_move(move)
-      reduce_possible_moves(move)
-    end
-
-    def wins?
-      WIN_COMBINATIONS.any? { |win| @past_moves.permutation(3).include?(win) }
-    end
-
-    def self.ties?
-      @@possible_moves.empty?
-    end
-
-    private
-
-    def save_move(move)
-      @past_moves.push(move)
-    end
-
-    def reduce_possible_moves(move)
-      @@possible_moves -= [move]
-    end
-
-    def legal_move?(move)
-      return true if @@possible_moves.include?(move)
-
-      puts "\nPlease enter an appropriate number"
-    end
+  def wins?
+    WIN_COMBINATIONS.any? { |win| @past_moves.permutation(3).include?(win) }
   end
 
-  # Human can pick their own moves
-  class Human < Player
-    def make_a_move(move)
-      return unless super
-
-      Game.board[POSITIONS[move]] = 'X'
-    end
+  def self.ties?
+    @@possible_moves.empty?
   end
 
-  # Computer picks a random move
-  class Computer < Player
-    def make_a_move(move = @@possible_moves.sample)
-      return unless super
+  private
 
-      Game.board[POSITIONS[move]] = 'O'
-    end
+  def make_a_move(move)
+    return unless legal_move?(move)
+
+    save_move(move)
+    reduce_possible_moves(move)
+  end
+
+  def legal_move?(move)
+    return true if @@possible_moves.include?(move)
+
+    puts "\nPlease enter an appropriate number"
+  end
+
+  def save_move(move)
+    @past_moves.push(move)
+  end
+
+  def reduce_possible_moves(move)
+    @@possible_moves -= [move]
   end
 end
 
-human = Game::Human.new
-bot = Game::Computer.new
+# Humans can pick their own moves
+class Human < Player
+  def initialize
+    super
+    puts 'Enter your name :'
+    @player_name = gets.chomp
+  end
 
-puts 'Please enter a number between 1 and 9'
+  def make_a_move(move, mark)
+    return unless super(move)
 
-until human.wins? || bot.wins? || Game::Player.ties?
-  input = gets.chomp.to_i
-  bot.make_a_move if human.make_a_move(input) && !human.wins?
-  puts Game.board
+    @@board[POSITIONS[move]] = mark
+  end
 end
 
-if human.wins?
-  puts 'Incredible Victory!'
-elsif bot.wins?
-  puts 'You lost, nice try!'
-else
-  puts 'A Tie!'
+# Computer picks a random move
+class Computer < Player
+  def initialize
+    super
+    @player_name = 'Computer'
+  end
+
+  def make_a_move(move = @@possible_moves.sample)
+    return unless super
+
+    @@board[POSITIONS[move]] = 'O'
+  end
 end
+
+Game.new
